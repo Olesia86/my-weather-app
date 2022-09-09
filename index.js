b6d82cc661f64d481fe496cb9e52ba141317217d
Olesia86 <111666356+Olesia86@users.noreply.github.com>
2022-09-09 10:39:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ let month = months[now.getMonth()];
 h3.innerHTML = `${day} ${date} ${month}, ${year}`;
 
 function showForecast(response) {
-  console.log(response.data);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
@@ -17,7 +16,7 @@ function showForecast(response) {
   forecastHTML = forecastHTML + `
   <div class="col-2">
   <div class="card-body">
-  <h6 class="card-title">${forecastDay.list.dt}</h6>
+  <h6 class="card-title">${forecastDay.dt}</h6>
   <p class="temp">${forcastDay.temp}°C</p>
   <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png/>"
   alt=""
@@ -27,13 +26,11 @@ function showForecast(response) {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML)
 }
 function getForecast(coordinates) {
   let apiKey = "49299905f177ecc5c9f1da6f89238e56"
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
-  console.log(coordinates);
  }
 
  function submitBtn(event) {
