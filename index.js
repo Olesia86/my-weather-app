0adce747517feb2e70cbb41f2fc20124e1176e99
Olesia86 <111666356+Olesia86@users.noreply.github.com>
2022-09-10 12:21:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function showForecast(response) {
   <div class="card-body">
   <h6 class="card-title">${forecastDay.dt}</h6>
   <p class="temp">${forecastDay.temp}°C</p>
-  <img src= "http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png/>
+  <img src= "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png/>
   alt=""
   width="50"/>
     </div>
@@ -68,7 +68,7 @@ function showWeather(response) {
   h2.innerHTML = response.data.name;
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
-    "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    "src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
