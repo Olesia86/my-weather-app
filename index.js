a4973c7fe41cbccdf4cb44d0c16d37f125dd3c88
Olesia86 <111666356+Olesia86@users.noreply.github.com>
2022-09-12 02:04:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function getForecast(coordinates) {
   let apiKey = "49299905f177ecc5c9f1da6f89238e56"
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
-  console.log(getForecast);
+  console.log(apiUrl);
  }
 
  function submitBtn(event) {
