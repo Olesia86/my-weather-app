54063e98f2a4ab711d55f7a0670845162311b66a
Olesia86 <111666356+Olesia86@users.noreply.github.com>
2022-09-12 01:55:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function showForecast(response) {
   <p class="temp">${Math.round(forecastDay.temp)}°C</p>
   <img src= "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
   alt=""
-  width="100"/>
+  width="50"/>
     </div>
     </div>`;
     }
