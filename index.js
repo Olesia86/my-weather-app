91b6cfb52b3d270a9ef75aad9dad08ce4eef7ff6
Olesia86 <111666356+Olesia86@users.noreply.github.com>
2022-09-12 01:39:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function showForecast(response) {
   <div class="card-body">
   <h6 class="card-title">${formatDay(forecastDay.dt)}</h6>
   <p class="temp">${Math.round(forecastDay.temp)}°C</p>
-  <img src= "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png/>
+  <img src= "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png
   alt=""
   width="50"/>
     </div>
